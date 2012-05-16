b148bac3f515cabba30d984875390207bf48e81c
Mario Andrei Pantoja <marioandrei@gmail.com>
2012-05-16 13:40:12 +0200

    Rot13

--- query_test.py
+++ query_test.py
@@ -3,7 +3,6 @@
 import os
 import webapp2
 import jinja2
-import uri
 
 from google.appengine.ext import db
 
@@ -11,22 +10,21 @@ template_dir=os.path.join(os.path.dirname(__file__),'templates')
 jinja_env=jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 class Handler(webapp2.RequestHandler):
+	"""auxiliares"""
 	def write(self, *a, **kw):
 		self.response.out.write(*a,**kw)
-
 	def render_str(self, template, **params):
 		t=jinja_env.get_template(template)
 		return t.render(params)
-
 	def render(self, template,**kw):
 		self.write(self.render_str(template,**kw))
 
-class Post(db.Model):
-	"""Base de datos del Blog"""
+class Art(db.Model):
+	"""Bese de datos for Art"""
 	title=db.StringProperty(required=True)
-	text=db.TextProperty(required=True)
+	art=db.TextProperty(required=True)
 	created=db.DateTimeProperty(auto_now_add=True) #como en SI1!
-	permalink=db.IntegerProperty()
+
 
 class MainPage(Handler):
 	def render_index(self, title="", art="", error=""):
@@ -49,6 +47,4 @@ class MainPage(Handler):
 			error="we need both a title and some artwork"
 			self.render_index(title,art,error)  
 
-app = webapp2.WSGIApplication([('/', MainPage),
-	('/blog', MainPage),
-	('/blog/newpost', MainPage)], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
